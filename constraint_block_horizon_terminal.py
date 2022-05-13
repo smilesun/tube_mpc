755cb7ac19d6c8ae50548943d1df92de3aca268d
Xudong Sun <smilesun.east@gmail.com>
2022-05-13 16:42:19 +0200

    documentation to terminal constraint

--- constraint_block_horizon_terminal.py
+++ constraint_block_horizon_terminal.py
@@ -8,7 +8,6 @@ class ConstraintBlockHorizonTerminal():
     """
     def __init__(self, mat_terminal_inf_pos_inva,
                  mat_x_u,
-                 mat_state_ub,
                  mat_u_ub):
         """__init__.
         :param mat_q:
@@ -16,8 +15,6 @@ class ConstraintBlockHorizonTerminal():
         """
         self.mat_terminal_inf_pos_inva = mat_terminal_inf_pos_inva
         self.mat_u_ub = mat_u_ub
-        self.mat_state_ub = mat_state_ub
-        self.dim_sys = mat_state_ub.shape[1]
         self.dim_input = mat_u_ub.shape[1]
 
     def gen_block_terminal_state_inva_constraint(self, horizon):
@@ -25,6 +22,46 @@ class ConstraintBlockHorizonTerminal():
         positive invariant terminal set only need to apply on x_T
         [0, 0, 0, P, |0, 0, 0] *
         [x_{0:T-1}, x_T,  u_{0:T-1}]^T =Px_T<= ones(nrow(P),1)
+
+        How to generate this P? Note that this P only works on x,
+        u is a deterministic funciton of x. So the coupling of
+        x and u is based on K^{Riccati}.
+
+        Starting from x_N till infinity, supposed K_{Riccati} is used, then
+
+        - next state must be inside terminal set(positive invariant):
+        x^+ = Ax + BKx = (A+BK)x must be inside positive invariant set
+        this is satisfied by the positive invariant set with A_c = A+BK as
+        closed loop system matrix. That means, P is the positive invariant
+        set w.r.t. A_c=A+BK
+
+        - K^{Riccati}*x must be inside the control input constraint d^T u <=1:
+            i.e.   for x_{T:inft}: D(Kx) <=1, how to ensure?
+
+            - Note Cx+Du<=1 is the general coupled state-control constraint,
+            [C, D] can have multiple block rows.
+            - One row can be
+                [0, d^T][x, u]^T =d^T u<=1
+            - Another row can be
+                [c^T, 0][x^T,u^T]^T = c^T x <=1
+
+        - Since u=kx (only after x_T, inside the horizon, the control signal
+        can be arbitrary), this is equivalent to d^TKx<=1, this is new
+        constraint to $x$, in addition to c^T x <=1
+        - in general,  Cx+Du<=1 can be transformed to
+          Cx+DKx = [CI, DK]x <=1 : since u is uniquely defined by $x$, all
+          constraints on u can be represented by $x$.
+          so the constraint on $X$ should be:
+          - Cx+DKx = [CI, DK]x <=1
+          - Will the constraint [c^T, 0][x^T, u^T]^T = c^Tx <=1
+            be covered by [CI, DK]x=[C,D][I, K^T]^T<=1 ?
+            i.e. is the following two rows redundant w.r.t. each other?
+            [c^T, 0][I, K^T]^T x <= 1
+            [c^T, 0] x <= 1
+            Yes! since [c^T, 0][I, K^T]^T x = c^T x <=1
+
+        - Conclusion: the constraint for state will be altered by K^{Riccati}
+          Cx+DKx = [CI, DK]x <=1 : since u is uniquely defined by $x$
         """
         block_one_hot = np.zeros((1, 2*horizon+1))
         block_one_hot[horizon] = 1
@@ -33,19 +70,6 @@ class ConstraintBlockHorizonTerminal():
         block_b_terminal_ub = np.ones(
             self.mat_terminal_inf_pos_inva.shape[0], 1)
         return block_mat_terminal_ub, block_b_terminal_ub
-
-    def gen_block_terminal_state_control_coupling(self, horizon, k_riccati):
-        """
-        starting from x_N till infinity, supposed k_riccati is used, then
-        - next state must be inside terminal set(positive invariant):
-        x^+ = Ax + BKx = (A+BK)x must be inside positive invariant set
-        this is satisfied by the positive invariant set
-        - k_riccati*x must be inside the control input
-        control constraint: for x_{T:inft}: C(Kx) <=1, how to ensure?
-        since x_{N+1} = Ax_T + BKx_T = (A+BK)x_T, so
-        C(A+BK)x_T<=1
-        """
-        pass
 
     def __call__(self, horizon,
                  mat_dyn_eq,  # functional constraint
